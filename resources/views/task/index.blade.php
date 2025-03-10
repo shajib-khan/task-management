1288ea1885f6f8794c773a95dd81d6e7b7a10c19
shajib-khan <shajibkhan946@gmail.com>
2025-03-10 15:03:12 +0600

    breeze install

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,6 +1,22 @@
 @extends('task.master')
 
 @section('content')
+
+<div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    @if (Route::has('login'))
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10 mt-3">
+            @auth
+                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+</div>
 @if(session()->has('success'))
 <div class="alert alert-success">
   {{session()->get('success')}}
@@ -34,7 +50,7 @@
       <tbody>
         @foreach ($tasks as $task )
         <tr>
-          
+
           <td>{{$task->id}}</td>
           <td>{{$task->title}}</td>
           <td>{{$task->description}}</td>
@@ -45,10 +61,10 @@
             <a href="{{route('edit.task', $task->id)}}" class="btn btn-primary">Edit</a>
             <a href="{{route('delete.task',$task->id)}}" class="btn btn-danger">Delete</a>
           </td>
-          
-    
 
-          
+
+
+
         </tr>
         @endforeach
       </tbody>
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProfileController;
 
 //Route::get('/', function () {
-  //  return view('welcome');
+ //   return view('welcome');
 //});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 
+//custom route
 Route::get('/',[TaskController::class, 'index'])->name('task.view');
 Route::get('/task-create',[TaskController::class,'create'])->name('task.create');
 Route::Post('/task-store',[TaskController::class,'store'])->name('store.task');
 Route::get('/edit-task/{task}',[TaskController::class,'edit'])->name('edit.task');
 Route::post('/update-task/{task}',[TaskController::class,'update'])->name('update.task');
 Route::get('/delete-task/{task}',[TaskController::class,'delete'])->name('delete.task');
-
-
