f146ae7d064adfd0931011e8e2e314c0b91aca4d
shajib-khan <shajibkhan946@gmail.com>
2025-03-15 11:51:33 +0600

    project done with multi authentication system

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Admin') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user')">
+                        {{ __('User') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
